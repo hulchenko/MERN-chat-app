bdd81b20cfaa910b8ee95143e3eb024b593ad171
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-01-13 19:22:48 -0400

    add toaster for failed websocket connections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import socket from "./socket";
+import toast, { Toaster } from "react-hot-toast";
 
 export const App = () => {
+  socket.on("connect_error", (err) => {
+    console.error(err);
+    toast.error("Websocket disconnected."); // TODO add throttle to display toast once every 30 seconds
+  });
+
   return (
-    <div>
-      {/* Nav */}
+    <>
+      <Toaster />
       <Outlet />
-      {/* Footer */}
-    </div>
+    </>
   );
 };
