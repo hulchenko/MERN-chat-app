6a85b06425fd8d0dbb06a58aa60b693ad47cabc9
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-01-14 01:22:35 -0400

    add new socket event listener for private messaging

--- server/config/websocket.js
+++ server/config/websocket.js
@@ -14,17 +14,30 @@ const websocketConnect = (server) => {
 
   io.on("connection", (socket) => {
     console.log("Websocket connected. ID: ", socket.id);
+
+    // Get all online users initially
     const connections = io.of("/").sockets;
     const users = [];
     console.log("Users online: ", connections.size);
-
     for (const [socketId, socketObj] of connections) {
       users.push({ id: socketId, username: socketObj.username });
     }
-
     socket.emit("initial_users", users);
 
-    socket.broadcast.emit("new_user", { id: socket.id, username: socket.username }); // announce to all connections
+    // Notify all connections with new users
+    socket.broadcast.emit("new_user", { id: socket.id, username: socket.username });
+
+    // Tet-a-tet messaging
+    socket.on("private_message", (data) => {
+      const { recipient, content } = data;
+      console.log("CURRENT USERS: ", users);
+      console.log(`Private message: Sending '${content}' from ${socket.id} to ${recipient}`);
+      io.to(recipient.id).emit("private_message", {
+        // emit message to the private room(user id)
+        content,
+        from: socket.id, //TODO is this accurate?
+      });
+    });
 
     socket.onAny((event, ...args) => {
       // listen to all events
